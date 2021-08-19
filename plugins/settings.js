f41c40c3260ea7e281fc7525ff54344834f495bf
root <cupuyc@gmail.com>
2021-08-19 12:13:34 +0300

    Settings import/export to file

--- plugins/settings.js
+++ plugins/settings.js
@@ -3,6 +3,10 @@
    	let idx = PROJECT + ''
 	let nav = document.querySelector('nav')
 	let cnt = document.querySelector('.container')
+   	let	file = document.createElement('input')
+        file.type = 'file'
+        file.accept ="text/*"
+
 	let storage = window.localStorage
     let btn = document.createElement('a')
         btn.href = '#' + idx
@@ -27,58 +31,64 @@
             }
         }
 	`)
-    let alert = showAlert(sec,{title:'Export your local storage.',message:''},false)
-    let alertImport = showAlert(sec,{title:'Import your data here.',message:'\n\n\n'},false)
+    let alert = showAlert(sec,{title:'',message:''},false)
+    let alertImport = showAlert(sec,{title:'',message:'\n\n\n'},false)
     let alertImportTitle = alertImport.querySelector('.alertHead')
     let alertImportBody = alertImport.querySelector('.alertBody')
         alertImportBody.classList.add('preformatted')
-        alertImportBody.contentEditable = 'true'
-        alertImportBody.style.outline = 'none'
+
+    let btnImport = document.createElement('i')
+        btnImport.className = 'fas fa-file-upload'
+        alertImportTitle.appendChild(btnImport)
 
     let alertTitle = alert.querySelector('.alertHead')
     let alertBody = alert.querySelector('.alertBody')
     let alertBodyTextNode = document.createTextNode('')
     let btnCopy = document.createElement('i')
         btnCopy.className = 'fas fa-copy'
-        alertBody.appendChild(btnCopy)
+        alertTitle.appendChild(btnCopy)
 
-
-    let copy = data => {
-        navigator.clipboard.writeText(data).then(() => {
-            alertBodyTextNode.remove()
-            alertBodyTextNode = document.createTextNode('Copying local storage to clipboard was successful!')
-            alertBody.appendChild(alertBodyTextNode)
-        }, err => {
-            d(['Async: Could not copy text: ', err]);
-        })
-    }
-
-    let tryToImport = e => {
-        let el = e.target
-        let data = false
-        try{
-            data = JSON.parse(el.innerText)
-        }catch(ee){
-            d(['failed first decoding, attempt',ee])
-            try{
-                data = JSON.parse(decodeURIComponent(window.atob(el.innerText)))
-            }catch(eee){
-                d(['failed seccond decoding attempt',eee])
-            }
-        }
-        if(data){
-            el.style.background = 'green'
-            Object.keys(data).forEach( k => localStorage.setItem(k, data[k]) )
-        }else{
-            el.style.background = 'red'
-        }
+    let download = (filename, text) => {
+        var element = document.createElement('a')
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
+        element.setAttribute('download', filename)
+        element.style.display = 'none';
+        document.body.appendChild(element)
+        element.click();
+        document.body.removeChild(element)
     }
 
     let exportStorage = e => {
-        copy(window.btoa(encodeURIComponent(JSON.stringify(storage, null, 4))))
+        download('reekol-xport-' + Date.now(), JSON.stringify(storage, null, 4))
     }
 
-        btnCopy.addEventListener('pointerdown', exportStorage, false)
-        alertImportBody.addEventListener('input', tryToImport, false)
+	let btnclick = e => {
+		file.click()
+	}
+
+	let filechange = e => {
+        let reader = new FileReader();
+            reader.onload = () => {
+                let data = false
+                alertImportBody.innerText = ''
+                try{ data = JSON.parse(reader.result) }catch(e){ d(e) }
+                if(data){
+                    alertImportTitle.style.background = 'green'
+                    Object.keys(data).forEach( k => {
+                        localStorage.setItem(k, data[k])
+                        alertImportBody.innerText += `Imported: ${k}\n`
+                    })
+                }else{
+                    alertImportTitle.style.background = 'red'
+                }
+            }
+        reader.readAsText(file.files[0])
+		file.value=null
+		file.files=null
+	}
+
+    file.      addEventListener('change',      filechange,     false)
+    btnImport. addEventListener('click',       btnclick,       false)
+    btnCopy.   addEventListener('pointerdown', exportStorage,  false)
 })()
 
