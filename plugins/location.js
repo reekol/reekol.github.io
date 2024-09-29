eaa951281906ecec89a9333e286b4053f3df5f01
reekol <cupuyc@gmail.com>
2024-09-29 21:44:49 +0300

    Style location.js

--- plugins/location.js
+++ plugins/location.js
@@ -1,19 +1,44 @@
 (() => {
 
 
- 	let cnt = document.querySelector('.container')
+	let nav = document.querySelector('nav')
+	let cnt = document.querySelector('.container')
+	let storage = window.localStorage
+
+	loadCss(`
+        @media (orientation: landscape) {
+            .container {
+                padding-top: 2vh
+            }
+            .map{
+            }
+
+        }
+        @media (orientation: portrait) {
+            .container {
+                padding-top: 8vw
+            }
+        }
+
+        .map {
+            color: #074450;
+        }
+	`)
 
   loadCssSrc("https://unpkg.com/leaflet@1.9.4/dist/leaflet.css")
   loadScript("https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",() => {
+
 
     let map = showAlert(cnt,{title:'Map.',message:'Loading...'},false)
     let mapTitle = map.querySelector('.alertHead')
     let mapBody = map.querySelector('.alertBody')
         mapBody.style.height = "300px"
         mapBody.id = 'map'
+        mapBody.classList.add('map')
 
     navigator.geolocation.getCurrentPosition(GeoPposition => {
       position = `Lat: ${GeoPposition.coords.latitude} / Lon: ${GeoPposition.coords.longitude}`
+      mapTitle.innerText = position
       console.log(position)
       const map = L.map('map').setView([GeoPposition.coords.latitude, GeoPposition.coords.longitude], 13);
       const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -22,7 +47,7 @@
       }).addTo(map);
 
       const marker = L.marker([GeoPposition.coords.latitude, GeoPposition.coords.longitude]).addTo(map)
-        .bindPopup(`<b>Position:</b><br />${position}`).openPopup();
+//        .bindPopup(`<b>Position:</b><br />${position}`).openPopup();
     });
 
   })
