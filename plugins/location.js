b4ef448952f9629ddf239e662556ad4083df5752
reekol <cupuyc@gmail.com>
2024-09-29 23:00:03 +0300

    Updates

--- plugins/location.js
+++ plugins/location.js
@@ -51,7 +51,7 @@
       const marker = L.marker([GeoPposition.coords.latitude, GeoPposition.coords.longitude]).addTo(map)
 //        .bindPopup(`<b>Position:</b><br />${position}`).openPopup();
     }, console.log, {
-      enableHighAccuracy: true,
+      enableHighAccuracy: false,
       timeout: 10000,
       maximumAge: 0,
     });
